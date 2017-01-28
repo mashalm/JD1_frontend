730cc4fea2e3b853d4f450fb9405f66dd9138c11
Rob Townsend <rtownsend2001@gmail.com>
2017-01-28 02:38:57 -0500

    separate client code

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -20,4 +20,36 @@ $('#loggedin').click(function() {
 $('#submitZipBtn').click(function() {
 	$('#introAudiologists').hide();
 	$('#audioResZip').show();
-})
+});
+
+$('#signUpButton').click(function() {
+    //we'll want to validate the form entry when clicked
+    var signUpData = {
+        'id' : generateUniqueId(),
+        'facebook_id': "000",
+        'email' : $('#emailSignUp').val(),
+        'password' : $('#passwordSignUp').val(),
+        'security_question' : $('#securityQuestionSignUp').val(),
+        'security_answer': $('securityAnswerSignUp').val(),
+        'dob' : 01-01-1965,
+        'tests' : null
+    };
+    $.ajax( {
+        type : 'POST',
+        url : 'serverurlgoeshere',
+        data : signUpData,
+        dataType : 'json',
+        encode : true,
+        success : function() {
+            alert('success');
+        },
+        
+    });
+});
+
+function generateUniqueId() {
+    //get epoch of this current second
+    var millis = (new Date).getTime();
+    var uniqueId = millis + Math.floor(Math.random() * 26);
+    return uniqueId;
+}
