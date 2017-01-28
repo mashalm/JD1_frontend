d3fa31e03b0efa55056d49eafe37c62037e7a137
mashalm <mashal.mannan@gmail.com>
2017-01-27 21:17:13 -0500

    added ajax to backend

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,28 @@ $('#loggedin').click(function() {
 $('#submitZipBtn').click(function() {
 	$('#introAudiologists').hide();
 	$('#audioResZip').show();
-})
+});
+
+$('#signUpButton').click(function() {
+    var signUpData = {
+        'id' : generateUniqueId(),
+        'facebook_id': "000",
+        'email' : $('#emailSignUp').val(),
+        'password' : $('#passwordSignUp').val(),
+        'security_question' : $('#passwordSignUp').val(),
+        'security_answer': $('securityAnswerSignUp').val(),
+        'dob' : 01-01-1965,
+        'tests' : null
+    };
+    $.ajax( {
+        type : 'POST',
+        url : 'serverurlgoeshere',
+        data : signUpData,
+        dataType : 'json',
+        encode : true,
+        success : function() {
+            alert('success');
+        },
+        
+    });
+});
