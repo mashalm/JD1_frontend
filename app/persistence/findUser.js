5cfe1faa5de68b2d4d88775ef669c65b9d24d89b
Rob Townsend <rtownsend2001@gmail.com>
2017-02-19 14:22:19 -0500

    update npm run server cmd; add error checks

--- app/persistence/findUser.js
+++ app/persistence/findUser.js
@@ -15,7 +15,6 @@ var findUserById = function(id) {
     .getItem(query)
     .promise()
     .then(function(response) {
-      console.log('response: ', response);
       return response;
     })
     .catch(function(err) {
@@ -25,7 +24,6 @@ var findUserById = function(id) {
 };
 
 var findUserByEmail = function(email) {
-  console.log('executing find by email');
   var query = {
     TableName: table,
     ExpressionAttributeValues: {
@@ -38,11 +36,13 @@ var findUserByEmail = function(email) {
     .scan(query)
     .promise()
     .then(function(response) {
-      console.log('response: ', response);
-      if (response.Count === 1)
-        return attr.unwrap(response.Items[0]);
+      if (response.Count === 0)
+        throw new Error('no user with that email exists');
 
-      else throw new Error('multiple users have the same email');
+      if (response.Count > 1) // shouldn't be reached
+        throw new Error('multiple users with that email exist');
+
+      return attr.unwrap(response.Items[0]);
     })
     .catch(function(err) {
       console.log('err', err);
